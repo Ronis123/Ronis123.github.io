c0d55bb01076d2989bc269f523b1f34c01bcb28f
Ronis <65214437+Ronis123@users.noreply.github.com>
2024-11-08 21:38:49 -0300

    Update script.js
    Adding second carousel

--- script.js
+++ script.js
@@ -2,24 +2,61 @@ let currentImageIndex = 0;
 const images = document.querySelectorAll(".galery .fotos img");
 const totalImages = images.length;
 
-// Função para esconder todas as imagens
+// Function to hide all images 
 function hideAllImages() {
     images.forEach(img => img.style.display = "none");
 }
 
-// Função para mostrar a próxima imagem
+// Function to show the next image
 function showNextImage() {
     hideAllImages();
     images[currentImageIndex].style.display = "block";
     currentImageIndex = (currentImageIndex + 1) % totalImages;
 }
 
-// Inicia o carrossel automaticamente a cada 2 segundos
+// Starting the Carousel automatically in two second
 setInterval(showNextImage, 2000);
 
-// Exibe a primeira imagem ao carregar a página
+// Display the first image to load the page
 document.addEventListener("DOMContentLoaded", function() {
     hideAllImages();
     showNextImage();
+});
+
+// Starting the Carousel 2
+let currentImageIndexNew = 0;
+const imagesNew = document.querySelectorAll(".fotos-novas .carousel-image");
+const totalImagesNew = imagesNew.length;
+
+// Function to hide all images
+function hideAllImages() {
+    imagesNew.forEach(img => img.style.display = "none");
+
+}
+
+// Function to image atual show 
+function showImage(index) {
+    hideAllImages();
+    imagesNew[index].style.display = "block";
+}
+
+// Function to display next image
+function nextImage() {
+    currentImageIndexNew = (currentImageIndexNew + 1) % totalImagesNew;
+    showImage(currentImageIndexNew);
+}
+
+// Function to display old image
+function prevImage() {
+    currentImageIndexNew = (currentImageIndexNew - 1 + totalImagesNew) % totalImagesNew;
+    showImage(currentImageIndexNew);
+}
+
+// Starting the carousel automatically the each 2 second.
+setInterval(nextImage, 2000);
+
+// Display the first image to load page.
+document.addEventListener("DOMContentLoaded", function() {
+    showImage(currentImageIndexNew);
 });
 
